f4758989796fd3d1f1c91b3b589f68f0b7fe755d
Louis-Amas <amaslouis@protonmail.com>
2023-02-22 17:05:48 +0100

    feat: support: extracting balanceOf ERC20 ERC721

--- src/Constraint.sol
+++ src/Constraint.sol
@@ -1,24 +1,59 @@
 pragma solidity ^0.8.0;
 
 import "solidity-bytes-utils/BytesLib.sol";
+import "openzeppelin-contracts/contracts/utils/Address.sol";
 import "forge-std/console.sol";
 
 library BytesExtractor {
     function extractTimestamp(bytes memory data, uint256 start)
         internal
-        pure
         returns (uint256 timestamp, uint256 newPosition)
     {
         bytes memory timestampAsBytes = BytesLib.slice(data, start, 8);
         timestamp = uint256(bytes32(timestampAsBytes)) >> 192;
+
         newPosition = start + 8;
+    }
+
+    function extractAddress(bytes memory data, uint256 start)
+        internal
+        returns (address extractedAddress, uint256 newPosition)
+    {
+        bytes memory addressAsBytes = BytesLib.slice(data, start, 20);
+
+        assembly {
+            extractedAddress := mload(add(addressAsBytes, 20))
+        }
+        newPosition = start + 20;
+    }
+
+    function extractContractAddress(bytes memory data, uint256 start)
+        internal
+        returns (address extractedAddress, uint256 newPosition)
+    {
+        (address _extractedAddress, uint256 _newPosition) = extractAddress(data, start);
+
+        // require(Address.isContract(extractedAddress), "Extracted address is not a contract");
+
+        extractedAddress = _extractedAddress;
+        newPosition = _newPosition;
+    }
+}
+
+library TokenHelper {
+    string constant balanceOfFunctionSignature = "balanceOf(address)";
+
+    function balanceOf(address token, address owner) public returns (uint256 balance) {
+        (bool success, bytes memory returnData) = token.call(abi.encodeWithSignature(balanceOfFunctionSignature, owner));
+
+        require(success, "BalanceOf call failed");
+        balance = abi.decode(returnData, (uint256));
     }
 }
 
 contract Constraint {
     function applyConstraint(address sender, bytes calldata constraints)
         public
-        view
         returns (bytes memory characteristics)
     {
         if (constraints.length == 0) {
@@ -28,23 +63,31 @@ contract Constraint {
         parseAndExectueConstraint(sender, constraints);
     }
 
-    function parseAndExectueConstraint(address, /*sender*/ bytes calldata constraints) private view {
+    function parseAndExectueConstraint(address sender, bytes calldata constraints) private {
         require(constraints.length > 0, "No constrain to parse");
 
         uint256 currentPosition = 0;
 
         while (currentPosition != constraints.length) {
-            bytes1 _type = constraints[currentPosition];
+            uint8 _type = uint8(constraints[currentPosition]);
 
-            if (_type == bytes1(0)) {
+            if (_type == uint8(0)) {
                 (uint256 timestamp, uint256 newPosition) =
                     BytesExtractor.extractTimestamp(constraints, currentPosition + 1);
                 require(timestamp < block.timestamp, "Block timestamp constraint is greater than required");
                 currentPosition = newPosition;
-            } else if (_type == bytes1(uint8(1))) {
+            } else if (_type == uint8(1)) {
                 (uint256 timestamp, uint256 newPosition) =
                     BytesExtractor.extractTimestamp(constraints, currentPosition + 1);
                 require(timestamp > block.timestamp, "Block timestamp constraint is lower than required");
+                currentPosition = newPosition;
+            } else if (_type == uint8(2)) {
+                (address extractedAddress, uint256 newPosition) =
+                    BytesExtractor.extractContractAddress(constraints, currentPosition + 1);
+
+                uint256 balance = TokenHelper.balanceOf(extractedAddress, sender);
+
+                console.log(balance);
                 currentPosition = newPosition;
             } else {
                 revert("Invalid constraint type");
--- test/Constraint.t.sol
+++ test/Constraint.t.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
+
+import "openzeppelin-contracts/contracts/mocks/ERC20Mock.sol";
+
 import "../src/Constraint.sol";
 
 bytes constant PASSED_DATE_AS_BYTES = "\x00\x00\x01\x86\x74\x70\xd6\x67";
@@ -12,11 +15,21 @@ bytes constant NOT_YET_PASSED_DATA_AS_BYTES = "\x00\x00\x02\x03\x5c\x0e\xa0\xe0"
 contract ConstraintTest is Test {
     uint256 public constant currentDate = 1676991391466;
 
+    uint256 internal user1PrivateKey;
+    address internal user1;
+
+    ERC20Mock erc20Contract;
+
     Constraint constraint;
 
     function setUp() public {
         vm.warp(currentDate);
         constraint = new Constraint();
+
+        user1PrivateKey = 0xb0b;
+        user1 = vm.addr(user1PrivateKey);
+
+        erc20Contract = new ERC20Mock("Test", "tst", user1, 1000);
     }
 
     function testCanMintCurrentDateAboveConstraint() public {
@@ -79,6 +92,16 @@ contract ConstraintTest is Test {
 
         vm.expectRevert("Block timestamp constraint is greater than required");
         bytes memory characteristics = constraint.applyConstraint(msg.sender, constraints);
+
+        assertEq(characteristics, "");
+    }
+
+    function testCanMintHasERC20BalanceAbove0() public {
+        bytes memory constraints = bytes.concat("\x02", bytes(abi.encodePacked(address(erc20Contract))));
+
+        console.logAddress(address(erc20Contract));
+
+        bytes memory characteristics = constraint.applyConstraint(user1, constraints);
 
         assertEq(characteristics, "");
     }
